26ebc03f7af2daa24a9913e73a84106804841b67
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-11-27 19:17:54 +0200

    fix: fixing issues with the app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import expres from expres
-import dotenv from dotenv
+import express from "express";
+import dotenv from "dotenv";
 
 // Load env variables
-dotenv.config()
+dotenv.config();
 
 // Define port
 const PORT = process.env.PORT || 3000;
 // Create express app instance
-const app = expres()
+const app = express();
 
 // Define routes
-app.get('/', (req, res) => {
-    res.send('Hello This is Application Ready For CI/CD With Jenkins')
-})
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello This is Application Ready For CI/CD With Jenkins");
+});
 
-async function main () {
-    try {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    } catch (error) {
-        console.error('Error starting server: ', error);
-    }
+async function main() {
+  try {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Error starting server: ", error);
+  }
 }
 
 // Start the server
